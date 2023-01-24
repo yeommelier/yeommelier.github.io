71223c00bda315b669bbe1a51674f05ca0174b76
yeommelier <yeojin.hwang.tech@gmail.com>
2023-01-24 11:01:24 +0900

    theme

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230124.105521';
+const cacheName = 'chirpy-20230124.105852';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
