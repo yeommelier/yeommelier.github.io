49700f2d9bbbc52378a12912434a909830df2317
yeommelier <yeojin.hwang.tech@gmail.com>
2023-01-24 18:16:19 +0900

    theme

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230124.181237';
+const cacheName = 'chirpy-20230124.181551';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
